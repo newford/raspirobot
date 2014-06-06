e18c160d40be69f510e418b62b31c8bad0bc3326
Henrik Nyberg <henriknyberg@me.com>
2014-06-07 00:18:46 +0200

    Changes in function drive_on to improve turning. Added input distance, but not used yet.

--- rover_run5.py
+++ rover_run5.py
@@ -17,7 +17,7 @@ HALF_SPEED = 12
 MIN_SPEED = 1
 MAX_SPEED = 16
 ZERO_SPEED = 0
-START_SPEED = 4
+START_SPEED = 6
 
 
 rr = RaspiRobot()
@@ -46,7 +46,7 @@ speed2 = 0  # right wheel, facing front
 def update_distance(status = STOP):
     dist = get_range()
     if dist == 0:
-        return
+        return(dist)
     message = 'Distance: ' + str(dist) + ' cm'
     status_message = 'Status: ' + status
     text_surface = font.render(message, True, (150, 150, 150))
@@ -62,6 +62,7 @@ def update_distance(status = STOP):
     pygame.draw.rect(screen, (0, 255, 0), Rect((10, 10),(w, 50)))    
     pygame.draw.rect(screen, (255, 0, 0), Rect((10, 10),(proximity, 50)))
     pygame.display.update()
+    return(dist)
 
 def get_range():
     try:
@@ -173,21 +174,21 @@ def go_right_pwm(left_duty = 0, alt_right_duty = 0):
           if j == 199:
               print('Left: ' + str(left_duty * (left_duty * go > i)) + 'Right: ' + str(right_duty * (right_duty * go > i)))
 
-def  drive_on(status,hold_time = 0.1, speed1 = 0, speed2 = 0):
+def  drive_on(status,hold_time = 0.1, speed1 = 0, speed2 = 0, dist):
     current_time = time.clock()
     new_time = current_time
     # TODO: fixme
+    # use input dist to alter speed
     while new_time < current_time + hold_time:
         # must keep motors going
-        if speed > 0 and speed < MAX_SPEED:
-            if status == FORWARD:
-                go_forward_pwm(speed1, speed2)
-            elif status == BACKWARD:
-                go_backward_pwm(speed1, speed2)
-            elif status == LEFT:
-                go_left_pwm(START_SPEED)
-            elif status == RIGHT:
-                go_right_pwm(START_SPEED)
+        if status == FORWARD:
+            go_forward_pwm(speed1, speed2)
+        elif status == BACKWARD:
+            go_backward_pwm(speed1, speed2)
+        elif status == LEFT:
+            go_left_pwm(HALF_SPEED)
+        elif status == RIGHT:
+            go_right_pwm(HALF_SPEED)
         new_time = time.clock()
 
 
@@ -227,6 +228,7 @@ def go_left():
     rr.left()
 
 print('Welcome to Raspirobot!')
+dist_meas = update_distance(status)
 
 while True:
     key_action = False
@@ -258,7 +260,7 @@ while True:
                 status = BACKWARD
             elif event.key == K_RIGHT:
                 print('RIGHT. Speed = ' + str(speed) + 'status: ' + status)
-                go_right_pwm(START_SPEED)
+                go_right_pwm(HALF_SPEED)
                 if status != RIGHT:
                     speed = 0
                 speed = 0
@@ -267,7 +269,7 @@ while True:
                 status = RIGHT
             elif event.key == K_LEFT:
                 print('LEFT. Speed = ' + str(speed) + 'status: ' + status)
-                go_left_pwm(START_SPEED)
+                go_left_pwm(HALF_SPEED)
                 if status != LEFT:
                     speed = 0
                 speed = 0
@@ -285,7 +287,7 @@ while True:
                 pygame.event.post(pygame.event.Event(QUIT))
             elif event.key == ord('a'):
                 #go left
-                print('Turn left, speed = ' + str(speed) + 'status: ' + status)
+                print('Hold left, speed = ' + str(speed) + 'status: ' + status)
                 speed = max(START_SPEED,speed)
                 speed1 = speed
                 speed2 = speed/2
@@ -392,6 +394,7 @@ while True:
 #                elif status == BACKWARD:
 #                    go_backward_pwm(speed1, speed2)
 
-    update_distance(status)
-    # time.sleep(0.1)
-    drive_on(status, TIME_STEP, speed1, speed2)
+    dist_meas_new = update_distance(status)
+    if dist_meas_new > 0:
+        dist_meas = dist_meas_new
+    drive_on(status, TIME_STEP, speed1, speed2, dist_meas)
